b53b33f17b7b4b15fa2ff04b5b13a9531a9146d9
pedrohenriqueads <pedrosilva@ads.fiponline.edu.br>
2022-04-12 21:25:17 -0300

    tasks-001-cenario1- configuracoes

---
+++ app/src/main/java/com/example/vou_nao_vou/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.vou_nao_vou.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
---
+++ app/src/main/java/com/example/vou_nao_vou/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package com.example.vou_nao_vou.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
